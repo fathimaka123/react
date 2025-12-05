0cf6999ce2cc96e6875817d5f4a7f1042a406c90
fathimaka123 <fathimaka0000@gmail.com>
2025-12-05 13:29:44 +0530

    deleted unwanted folders from git

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import ReactDOM from "react-dom/client"
 /* {
   /* <div id="parent">
@@ -12,16 +12,47 @@ import ReactDOM from "react-dom/client"
   </div>
 </div>; 
 } */
+const Hello=()=>{
+ return "This is a Component"
+}
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child",key:"1" }, [
-    React.createElement("h1", {key:"a"}, "Hello world from react"),
+const Greetings=()=>{
+  <div> <h1>component composition</h1>
+  <p>When you call a component in another component</p>
+  </div>
+}
+
+const Heading02=(
+  <div>
+    <Hello/>
+  </div>
+)
+console.log(Heading02);
+
+
+const heading=(
+  <div>
+    <h1>This is day3 react using jsx</h1>
+     <div>
+      <Greetings/>
+  </div>
+  </div>
+ 
+)
+console.log(heading)
+
+/* const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key:"1"}, [
+    React.createElement("h1",{key:"a"}, "Hello world from react"),
     React.createElement("h2", {key:"b"}, "This is h2 tag"),
   ]),
-  React.createElement("div", { id: "child2",key:"2" }, [
-    React.createElement("h1", {key:"c"}, "Hello world from react"),
-    React.createElement("h2", {key:"d"}, "This is h2 tag"),
+  React.createElement("div", { id: "child2" ,key:"2" }, [
+    React.createElement("h1",{key:"c"}, "Hello world from react-day2"),
+    React.createElement("h2", {key:"d"}, "This is h2 tag 2"),
   ]),
-]);
+]); */
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(heading);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(Heading02);
